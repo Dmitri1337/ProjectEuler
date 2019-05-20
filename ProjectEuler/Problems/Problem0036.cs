ccebca9198bce09feb05c8340288bb5b34cf6624
Dmitri1337 <dmitri.trofimov@prime-initiative.com>
2019-05-20 18:04:31 +0300

    Solved problem 36: Double-base palindromes

---
+++ ProjectEuler/Problems/Problem0036.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using ProjectEuler.Math;
+
+namespace ProjectEuler.Problems
+{
+    /// <summary>
+    ///     Double-base palindromes
+    /// </summary>
+    public class Problem0036
+    {
+        public object GetResult()
+        {
+            const int max = 1000000;
+
+            return Enumerable.Range(0, max)
+                .Where(x => Palindromes.IsPalindrome(x, 2) && Palindromes.IsPalindrome(x))
+                .Sum();
+        }
+    }
+}
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -9,7 +9,7 @@ namespace ProjectEuler
         {
             Console.Write("Calculating result...");
 
-            object result = new Problem0005().GetResult();
+            object result = new Problem0036().GetResult();
 
             Console.WriteLine(" done.");
             Console.WriteLine($"Result: {result}");
