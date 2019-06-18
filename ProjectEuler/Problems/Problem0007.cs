403722d23803e321fdb2bdac2b28537839783598
Dmitri1337 <dmitri.trofimov@prime-initiative.com>
2019-06-18 19:20:48 +0300

    Problem 7: 10001st prime

---
+++ ProjectEuler/Problems/Problem0007.cs
@@ -0,0 +1,26 @@
+﻿using ProjectEuler.Math;
+
+namespace ProjectEuler.Problems
+{
+    /// <summary>
+    ///     10001st prime
+    /// </summary>
+    public class Problem0007
+    {
+        public object GetResult()
+        {
+            int primeCount = 0;
+            int currentNumber = 1;
+
+            while (primeCount < 10001)
+            {
+                currentNumber++;
+
+                if (Primes.IsPrime(currentNumber))
+                    primeCount++;
+            }
+
+            return currentNumber;
+        }
+    }
+}
