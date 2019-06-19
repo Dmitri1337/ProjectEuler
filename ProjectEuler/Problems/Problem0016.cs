093aa722d7a4f061b13f4854be4ad0d9e214202f
Dmitri1337 <dmitri.trofimov@prime-initiative.com>
2019-06-19 14:03:28 +0300

    Problem 16: Power digit sum

---
+++ ProjectEuler/Problems/Problem0016.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Numerics;
+
+namespace ProjectEuler.Problems
+{
+    /// <summary>
+    ///     Power digit sum
+    /// </summary>
+    public class Problem0016
+    {
+        public object GetResult()
+        {
+            return BigInteger
+                .Pow(2, 1000)
+                .ToString()
+                .Select(x => x - 48)
+                .Sum();
+        }
+    }
+}
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -9,7 +9,7 @@ namespace ProjectEuler
         {
             Console.Write("Calculating result...");
 
-            object result = new Problem0015().GetResult();
+            object result = new Problem0016().GetResult();
 
             Console.WriteLine(" done.");
             Console.WriteLine($"Result: {result}");
