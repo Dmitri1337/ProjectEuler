999c0106b710e62b441586688d21e5b9f8b3d669
Dmitri Trofimov <dmitri.trofimov@gmail.com>
2019-06-19 19:28:03 +0300

    Problem 23: Non-abundant sums

---
+++ ProjectEuler/Problems/Problem0023.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using ProjectEuler.Math;
+
+namespace ProjectEuler.Problems
+{
+    /// <summary>
+    ///     Non-abundant sums
+    /// </summary>
+    public class Problem0023
+    {
+        public object GetResult()
+        {
+            const int max = 28123;
+
+            int[] abundantNumbers = Enumerable.Range(1, max).Where(IsAbundantNumber).OrderBy(x => x).ToArray();
+            int[] nonAbundantSums = Enumerable.Range(0, max + 1).ToArray();
+
+            for (int a = 0; a < abundantNumbers.Length; a++)
+            for (int b = a; b < abundantNumbers.Length; b++)
+            {
+                int abundantSum = abundantNumbers[a] + abundantNumbers[b];
+                if (abundantSum < nonAbundantSums.Length)
+                    nonAbundantSums[abundantSum] = 0;
+                
+            }
+
+            return nonAbundantSums.Sum();
+        }
+
+        private static bool IsAbundantNumber(int number)
+        {
+            return Primes.GetProperDivisors(number).Sum() > number;
+        }
+    }
+}
