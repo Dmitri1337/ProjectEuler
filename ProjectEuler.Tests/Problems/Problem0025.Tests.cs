4fba150d9f98c947e768c7a1b8413260b174bed0
Dmitri Trofimov <dmitri.trofimov@gmail.com>
2019-06-19 20:30:22 +0300

    Problem 25: 1000-digit Fibonacci number

---
+++ ProjectEuler.Tests/Problems/Problem0025.Tests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using ProjectEuler.Problems;
+
+namespace ProjectEuler.Tests.Problems
+{
+    [TestFixture]
+    public class Problem0025Tests
+    {
+        [TestFixture]
+        public class WhenGettingResult
+        {
+            [Test]
+            public void ShouldReturnCorrectResult()
+            {
+                var problem = new Problem0025();
+                object result = problem.GetResult();
+
+                result.Should().Be(4782);
+            }
+        }
+    }
+}
---
+++ ProjectEuler/Problems/Problem0025.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+
+namespace ProjectEuler.Problems
+{
+    /// <summary>
+    ///     1000-digit Fibonacci number
+    /// </summary>
+    public class Problem0025
+    {
+        public object GetResult()
+        {
+            BigInteger thousandDigits = BigInteger.Pow(10, 999);
+
+            BigInteger previousTerm = 0;
+            BigInteger currentTerm = 1;
+            int currentTermIndex = 1;
+
+            while (currentTerm < thousandDigits)
+            {
+                BigInteger newTerm = previousTerm + currentTerm;
+                previousTerm = currentTerm;
+                currentTerm = newTerm;
+                currentTermIndex++;
+            }
+
+            return currentTermIndex;
+        }
+    }
+}
